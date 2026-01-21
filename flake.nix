e920c410e2e52298ab614efd49c3a6758e34d863
soymou <emilio.junoy@gmail.com>
2026-01-21 13:48:05 -0600

    Added file manager to nix

--- flake.nix
+++ flake.nix
@@ -18,10 +18,13 @@
             libedit
             ncurses
             manix  # For NixOS options search
+            gtk3
+            gsettings-desktop-schemas
           ];
 
           shellHook = ''
             export LD_LIBRARY_PATH="${pkgs.libedit}/lib:${pkgs.ncurses}/lib:$LD_LIBRARY_PATH"
+            export XDG_DATA_DIRS="${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS"
             echo "Nix Workspace Manager environment loaded."
           '';
         };
